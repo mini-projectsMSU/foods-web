6942d34b7fde07a57e6ed5e4680791a7949b09c4
mini-projectsMSU <119317748+mini-projectsMSU@users.noreply.github.com>
2022-11-28 18:36:37 +0530

    Add files via upload

---
+++ food website backend/components/footer.php
@@ -0,0 +1,43 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:kushupadyay25@gmail.com">kushupadyay25@gmail.com</a>
+         <a href="mailto:yadavshaantanuu@gmail.com">yadavshaantanuu@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>11:00am to 11:00pm</p>
+         <br>
+         <br>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">MSU, vadodara - 390020</a>
+         <br>
+         <br>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">7698893789</a>
+         <a href="tel:1112223333">8238349775</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>coding cafe</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
